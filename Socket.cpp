b1065b2cf0308dd6d4e036f0542956d644ec3f68
caozhi <563579073@qq.com>
2024-05-08 16:33:14 +0800

    封装Socket

---
+++ Socket.cpp
@@ -0,0 +1,70 @@
+#include "Socket.h"
+// #include "stdio.h"
+//创建服务端监听socket
+int createListensocket(){
+    int listenfd = socket(AF_INET, SOCK_STREAM|SOCK_NONBLOCK, IPPROTO_TCP);
+    if(listenfd<0){//创建失败,程序退出
+        //log
+        exit(-1);
+    }
+    return listenfd;
+}
+
+Socket::Socket(int fd):m_fd(fd){
+    // printf("copy construct\n");
+};
+
+Socket::~Socket(){
+    ::close(m_fd);
+}
+
+int Socket::fd() const {
+    return m_fd;
+}
+
+void Socket::setTcpnodelay(bool on){
+    int optval = on ? 1 : 0;
+    ::setsockopt(m_fd, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval)); // TCP_NODELAY包含头文件 <netinet/tcp.h>
+}
+
+void Socket::setReuseaddr(bool on){
+    int optval = on ? 1 : 0;
+    ::setsockopt(m_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)); 
+}
+
+void Socket::setReuseport(bool on){
+    int optval = on ? 1 : 0;
+    ::setsockopt(m_fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)); 
+}
+
+void Socket::setKeepalive(bool on){
+    int optval = on ? 1 : 0;
+    ::setsockopt(m_fd, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval)); 
+}
+
+void Socket::bind(const InetAddress& servaddr){
+    if(::bind(m_fd, servaddr.addr(), sizeof(sockaddr))<0){
+        printf("bind error\n");
+        ::close(m_fd);
+        exit(-1);
+    }
+}
+
+void Socket::listen(int nums){
+    if(::listen(m_fd, nums) !=0 ){
+        //log error
+        printf("listen error\n");
+        ::close(m_fd);
+        exit(-1);
+    }
+}
+
+int Socket::accept(InetAddress& clientaddr){
+    sockaddr_in peeraddr;
+    socklen_t len = sizeof(peeraddr);
+    int clientfd = accept4(m_fd, (sockaddr*)&peeraddr, &len, SOCK_NONBLOCK);
+
+    clientaddr.setaddr(peeraddr);
+
+    return clientfd;
+}
---
+++ Socket.h
@@ -0,0 +1,38 @@
+#ifndef _SOCKET_H
+#define _SOCKET_H
+
+// #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/tcp.h>
+#include <string>
+// #include <errno.h>
+#include <unistd.h>
+#include "InetAddress.h"
+
+//创建服务端监听socket
+int createListensocket();
+
+/**
+ * @class:Socket
+ * @brief:封装socket相关数据
+*/
+
+class Socket{
+private:
+    const int m_fd;//socket的fd,文件描述符
+public:
+    Socket(int fd);
+    ~Socket();
+
+    int fd() const;
+    void setReuseaddr(bool);//设置地址重使用,取消time_wait
+    void setReuseport(bool);//设置端口重使用
+    void setTcpnodelay(bool);//禁用 Nagle 算法，即禁用 TCP 的延迟确认机制，数据发送时立即发送而不缓存
+    void setKeepalive(bool);//tcp保活
+
+    void bind(const InetAddress&);//绑定socket与ip:port
+    void listen(int nums = 128);//开启监听客户端连接
+    int accept(InetAddress&);//收到客户端连接
+};
+
+#endif
